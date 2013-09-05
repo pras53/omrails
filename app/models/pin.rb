f5b99ece97ec7e41e055b1176964112eb56444e6
pras53 <prasid.pathak@gmail.com>
2013-09-04 22:06:24 -0700

    Got it working again, re-do image stuff in pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,15 +1,9 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description
 
   validates :description, presence: true
-<<<<<<< HEAD
-	validates :user_id, presence: true
-	validates_attachment :image, presence: true, 
-															content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-															size: { less_than: 5.megabytes }
+
 
   belongs_to :user
-  has_attached_file :image
-=======
->>>>>>> parent of d11bce1... Add Pins Associations
+ 		validates :user_id, presence: true
 end
